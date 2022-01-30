c70c38683b13a534ebdabcda873f7e132e8e5098
Fillipi Pereira <pfillipi@gmx.de>
2022-01-30 14:37:26 +0100

    section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'PHiliPp')}}</title>
-    </head>
-    <body>
-      <h1>Om</h1>
-      <p>Dette er om-siden</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <h1>Om</h1>
+  <p>Dette er om-siden</p>
+@endsection
