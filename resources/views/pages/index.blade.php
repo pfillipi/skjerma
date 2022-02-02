5e2d93a704d6221111c1543151df09d6586cf144
Fillipi Pereira <pfillipi@gmx.de>
2022-02-02 14:32:50 +0100

    extends

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'PHiliPp')}}</title>
-    </head>
-    <body>
-      <h1>Velkommen til Laravel</h1>
-      <p>Dette er Laravel-applikasjonen fra Laravel fra bunnen av YouTube-serien</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <h1>Velkommen til Laravel</h1>
+  <p>Dette er Laravel-applikasjonen fra Laravel fra bunnen av YouTube-serien</p>
+@endsection
